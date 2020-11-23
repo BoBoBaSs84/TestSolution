6156f547bbc2cbf14c015b875154840f2f585dcc
Robert Peter Meyer <robert.rm.meyer@siemens.com>
2020-11-23 20:02:01 +0100

    BankAccount Tut done, next P.97

--- BankConsoleApp/GiftCardAccount.cs
+++ BankConsoleApp/GiftCardAccount.cs
@@ -8,8 +8,14 @@ namespace BankConsoleApp
 {
     public class GiftCardAccount : BankAccount
     {
-        public GiftCardAccount(string name, decimal initialBalance) : base(name, initialBalance)
+        private decimal _monthlyDeposit = 0m;
+        public GiftCardAccount(string name, decimal initialBalance, decimal monthlyDeposit = 0) : base(name, initialBalance) => _monthlyDeposit = monthlyDeposit;
+        public override void PerformMonthEndTransactions()
         {
+            if(_monthlyDeposit != 0)
+            {
+                MakeDeposit(_monthlyDeposit, DateTime.Now, "Add monthly deposit");
+            }
         }
     }
 }
