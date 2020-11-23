253d51c3389414cdb7e5529e1394356e1c7e038c
Robert Peter Meyer <robert.rm.meyer@siemens.com>
2020-11-23 20:05:14 +0100

    Starting Random Answer Console App

---
+++ RandomAnswerConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RandomAnswerConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
