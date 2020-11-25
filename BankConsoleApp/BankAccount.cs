0cb0950034c8bd0eca3ddc88d15619108857f923
Robert Peter Meyer <robert.rm.meyer@siemens.com>
2020-11-25 10:17:03 +0100

    warning, fix

--- BankConsoleApp/BankAccount.cs
+++ BankConsoleApp/BankAccount.cs
@@ -73,7 +73,9 @@ namespace BankConsoleApp
             if (overdraftTransaction != null)
                 allTransactions.Add(overdraftTransaction);
         }
+#pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
         protected virtual Transaction? CheckWithdrawalLimit(bool isOverdrawn)
+#pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
         {
             if (isOverdrawn)
             {
