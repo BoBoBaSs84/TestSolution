2fda639076741c54a81a41082bdc1f3d410c0781
Robert Peter Meyer <robert.rm.meyer@siemens.com>
2020-11-26 12:30:04 +0100

    well the SqlConsoleApp added ... and some config stuff

---
+++ SqlConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SqlConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
