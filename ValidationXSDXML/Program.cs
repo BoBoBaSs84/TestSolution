c5da5a68f45dc4b22b30dec8f77b56285b75c675
Robert Peter Meyer <robert.rm.meyer@siemens.com>
2020-12-03 14:25:21 +0100

    XSD & XML

---
+++ ValidationXSDXML/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ValidationXSDXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
