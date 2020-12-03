094876eb8cf31f41121ad8453f772a434e836be0
Robert Peter Meyer <robert.rm.meyer@siemens.com>
2020-12-03 15:21:26 +0100

    whoop whoop works ... with param

--- ValidationXSDXML/Program.cs
+++ ValidationXSDXML/Program.cs
@@ -1,31 +1,33 @@
-﻿using System;
-using System.IO;
-using System.Xml;
-using System.Xml.Linq;
-using System.Xml.Schema;
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
 
 namespace ValidationXSDXML
 {
+    [Command(Name = "XML XSD Validator", Description = "The application validates an XML against the given XSD")]
+    [HelpOption("-?")]
     class Program
     {
+        static void Main(string[] args) => CommandLineApplication.Execute<Program>(args);
 
-        static void Main(string[] args)
+        [Argument(0, Description = "XSD file")]
+        private string xsdfile { get; }
+        [Argument(1, Description ="XML file")]
+        private string xmlfile { get; }
+
+        private void OnExecute()
         {
-            var path = new Uri(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase)).LocalPath;
-            XmlSchemaSet schema = new XmlSchemaSet();
-            schema.Add("", path + "\\mk.xsd");
-            XmlReader rd = XmlReader.Create(path + "\\102865640.xml");
-            XDocument doc = XDocument.Load(rd);
-            doc.Validate(schema, ValidationEventHandler);
-        }
-        static void ValidationEventHandler(object sender, ValidationEventArgs e)
-        {
-            XmlSeverityType type = XmlSeverityType.Warning;
-            if (Enum.TryParse<XmlSeverityType>("Error", out type))
+            if(xsdfile == null)
             {
-                if (type == XmlSeverityType.Error) throw new Exception(e.Message);
+                Console.WriteLine("specify the xsd file ... better start with the parameter -?");
+                return;
             }
+            if (xmlfile == null)
+            {
+                Console.WriteLine("specify the xml file ... better start with the parameter -?");
+                return;
+            }
+            var validate = new Validation();
+            validate.Validate(xsdfile, xmlfile);
         }
-
     }
 }
---
+++ ValidationXSDXML/Validation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace ValidationXSDXML
+{
+    public class Validation
+    {
+        public void Validate(string p_strXSD, string p_strXML)
+        {
+            var path = new Uri(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase)).LocalPath;
+            XmlSchemaSet schema = new XmlSchemaSet();
+            schema.Add("", path + "\\" + p_strXSD);
+            XmlReader rd = XmlReader.Create(path + "\\" + p_strXML);
+            XDocument doc = XDocument.Load(rd);
+            doc.Validate(schema, ValidationEventHandler);
+        }
+        private void ValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            XmlSeverityType type = XmlSeverityType.Warning;
+            if (Enum.TryParse<XmlSeverityType>("Error", out type))
+            {
+                if (type == XmlSeverityType.Error) throw new Exception(e.Message);
+            }
+        }
+    }
+}
