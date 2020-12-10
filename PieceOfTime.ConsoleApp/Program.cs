4f6211bcd83ce7496013a625669293bf67c70bdb
Robert Peter Meyer <robert.rm.meyer@siemens.com>
2020-12-10 13:40:56 +0100

    PieceOfTime added

---
+++ PieceOfTime.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PieceOfTime.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
